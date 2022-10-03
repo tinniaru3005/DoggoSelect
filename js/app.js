728fa1b3c74a339e32558d79a82673af72c82325
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-10-03 11:06:36 +0530

    Add the options

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+fetch('https://dog.ceo/api/breeds/list')
+    .then(response => response.json())
+    .then(data => generateOptions(data.message));
+
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then((data) => generateImage(data.message));
@@ -13,6 +17,13 @@ fetch('https://dog.ceo/api/breeds/image/random')
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function generateOptions(data) {
+    const options = data.map(item => `
+        <option value = '${item}'>${item}</option>
+    `).join('');
+    select.innerHTML = options;
+}
+
 function generateImage(data) {
     const html = `
         <img src = '${data}' alt>
